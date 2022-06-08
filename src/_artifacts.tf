b188dbaa951e51ff779502be05c57423afdf4daf
Cory O'Daniel <cory@coryodaniel.com>
2022-06-07 17:51:08 -0700

    first commit

---
+++ src/_artifacts.tf
@@ -0,0 +1,37 @@
+locals {
+  data_authentication = {
+    username = aws_rds_cluster.main.master_username
+    password = aws_rds_cluster.main.master_password
+    hostname = aws_rds_cluster.main.endpoint
+    # replica_hostname = aws_rds_cluster.main.reader_endpoint
+    port = aws_rds_cluster.main.port
+  }
+  data_infrastructure = {
+    arn = aws_rds_cluster.main.arn
+  }
+
+  rdbms_specs = {
+    engine         = "MySQL"
+    engine_version = aws_rds_cluster.main.engine_version
+    version        = aws_rds_cluster.main.engine_version_actual
+  }
+}
+
+resource "massdriver_artifact" "authentication" {
+  field                = "authentication"
+  provider_resource_id = aws_rds_cluster.main.arn
+  type                 = "mysql-authentication"
+  name                 = "MySQL user credentials: ${aws_rds_cluster.main.cluster_identifier}"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = local.data_infrastructure
+        authentication = local.data_authentication
+        security       = {}
+      }
+      specs = {
+        rdbms = local.rdbms_specs
+      }
+    }
+  )
+}
